a0e3f515e374f2d84972992941ac49823554fc19
Danin Sadžak <daninsadzak@gmail.com>
2023-01-28 23:29:19 +0100

    Added plant abbreviation for Papilionoideae

--- bosnian.h
+++ bosnian.h
@@ -519,6 +519,7 @@ void grupaSupnice(std::string cvijet) {
 		else if (cvijet == "Spiraeoideae" || cvijet == "spiraeoideae" || cvijet == "Grmlje" || cvijet == "grmlje") supnica << "SPIRAEOIDEAE (GRMLJE) -> " << dvosupnica << '\n';
 		else if (cvijet == "Mimosoideae" || cvijet == "mimosoideae" || cvijet == "Mimozovke" || cvijet == "mimozovke") supnica << "MIMOSOIDEAE (MIMOZOVKE) -> " << dvosupnica << '\n';		
 		else if (cvijet == "Caesalpinioideae" || cvijet == "caesalpinioideae" || cvijet == "Sapanovke" || cvijet == "sapanovke") supnica << "CAESALPINIOIDEAE (SAPANOVKE) -> " << dvosupnica << '\n';
+		else if (cvijet == "Papilionoideae" || cvijet == "papilionoideae") supnica << "PAPILIONOIDEAE -> " << dvosupnica << '\n';
 		else supnica << "PORODICA NE POSTOJI\n";
  	}
 }
