1ac161adc88e67eb0579a123e7e90347b74d8d42
Danin Sadžak <daninsadzak@gmail.com>
2023-01-30 22:23:05 +0100

    Added the Iridaceae family

--- bosnian.h
+++ bosnian.h
@@ -663,6 +663,22 @@ void cvijetnaFormula(std::string cvijet)
 		std::cout << "_ (crta gore) = plodnica podrasla\n";
 		SetConsoleTextAttribute(h, 7);
 	}
+	else if (cvijet == "Iridaceae" || cvijet == "iridaceae" || cvijet == "Perunike" || cvijet == "perunike") {
+		HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+
+		SetConsoleTextAttribute(h, 10);
+		std::cout << "Grada porodice Iridaceae (perunike): \n\n";
+		std::cout << "          ---\n";
+		std::cout << "*P3+3 A3 G(3)\n\n";
+		SetConsoleTextAttribute(h, 4);
+		std::cout << "P = perigon\n";
+		std::cout << "A = prasnici\n";
+		std::cout << "G = plodnica obrasla\n";
+		std::cout << "P3+3 = broj ciklusa i clanova u ciklusu\n";
+		std::cout << "() = elementi srasli\n";
+		std::cout << "_ (crta gore) = plodnica podrasla\n";
+		SetConsoleTextAttribute(h, 7);
+	}
 	else {
 		std::cout<<"Ta porodica ne postoji. Pokusaj te ponovo.\n";
 	}
