8a65e84b08d73b98c961f255d4fccaeb04559a62
Danin Sadžak <daninsadzak@gmail.com>
2023-02-01 19:36:44 +0100

    Added images for Prunoideae

--- bosnian.h
+++ bosnian.h
@@ -149,8 +149,35 @@ void cvijetnaFormula(std::string cvijet)
 		std::cout << (char)236;
 		std::cout << " = veliki broj clanova u zavojnici\n";
 		std::cout << "G-1- = plodnica obrasla\n";
-		SetConsoleTextAttribute(h, 7);
 		supnica << "PRUNOIDEAE (SLJIVE) -> " << dvosupnica << '\n';
+		std::cout << '\n';
+		SetConsoleTextAttribute(h, 6);
+		int odluka;
+		std::cout << "Izaberite jedne od 3 ponudenih slika!\n";
+		std::cout << "1. Uzivo cvjet\n";
+		std::cout << "2. Plod cvjeta\n";
+		std::cout << "3. Graficki prikaz\n";
+		SetConsoleTextAttribute(h, 7);
+		again2:std::cin >> odluka;
+		std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> convert;
+		std::wstring url;
+		switch(odluka) {
+			case 1:
+  				url = convert.from_bytes("https://cdn.discordapp.com/attachments/594198772150698024/1070411755915858040/prunus-spinosa-prunus-spinosa-thorn-thorn-prickly-plum-small-prickly-shrub-species-genus-prunus-235085148.jpg");
+  				ShellExecuteW(NULL, L"open", url.c_str(), NULL, NULL, SW_SHOWNORMAL);
+				break;
+			case 2:
+				url = convert.from_bytes("https://cdn.discordapp.com/attachments/594198772150698024/1070411755504799884/images.jpg");
+				ShellExecuteW(NULL, L"open", url.c_str(), NULL, NULL, SW_SHOWNORMAL);
+				break;
+			case 3:
+				url = convert.from_bytes("https://cdn.discordapp.com/attachments/594198772150698024/1070411755118919710/Capture.PNG");
+				ShellExecuteW(NULL, L"open", url.c_str(), NULL, NULL, SW_SHOWNORMAL);
+				break;
+			default:
+				std::cout << "Molimo vas da izabere brojeve izmedju 1-3!\n";
+				goto again2;
+		}
 	} 
 	else if(cvijet == "Brassicaceae" || cvijet == "brassicaceae" || cvijet == "Krstasice" || cvijet == "krstasice") {
 		HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
