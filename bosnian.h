15f6e7c3b230e806516eb7de328c5b9302ce65f1
Danin Sadžak <daninsadzak@gmail.com>
2023-02-01 20:02:55 +0100

    Added images for Solonaceae

--- bosnian.h
+++ bosnian.h
@@ -374,8 +374,35 @@ void cvijetnaFormula(std::string cvijet)
 		std::cout << "A = prasnici\n";
 		std::cout << "G = plodnica obrasla\n";
 		std::cout << "() = elementi srasli\n";
-		SetConsoleTextAttribute(h, 7);
 		supnica << "SOLONACEAE (POMOCNICE) -> " << dvosupnica << '\n';
+		std::cout << '\n';
+		SetConsoleTextAttribute(h, 6);
+		int odluka;
+		std::cout << "Izaberite jedne od 3 ponudenih slika!\n";
+		std::cout << "1. Uzivo cvjet\n";
+		std::cout << "2. Plod cvjeta\n";
+		std::cout << "3. Graficki prikaz\n";
+		SetConsoleTextAttribute(h, 7);
+		again7:std::cin >> odluka;
+		std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> convert;
+		std::wstring url;
+		switch(odluka) {
+			case 1:
+  				url = convert.from_bytes("https://cdn.discordapp.com/attachments/594198772150698024/1070418431674945606/Brugmansia_lg.jpg");
+  				ShellExecuteW(NULL, L"open", url.c_str(), NULL, NULL, SW_SHOWNORMAL);
+				break;
+			case 2:
+				url = convert.from_bytes("https://cdn.discordapp.com/attachments/594198772150698024/1070418431997911101/Capture.PNG");
+				ShellExecuteW(NULL, L"open", url.c_str(), NULL, NULL, SW_SHOWNORMAL);
+				break;
+			case 3:
+				url = convert.from_bytes("https://cdn.discordapp.com/attachments/594198772150698024/1070418432257953864/ffff.PNG");
+				ShellExecuteW(NULL, L"open", url.c_str(), NULL, NULL, SW_SHOWNORMAL);
+				break;
+			default:
+				std::cout << "Molimo vas da izabere brojeve izmedju 1-3!\n";
+				goto again7;
+		}
 	}
 	else if(cvijet == "Asteraceae" || cvijet == "asteraceae" || cvijet == "Glavocike" || cvijet == "glavocike") {
 		HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
